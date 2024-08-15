c063c3b59df098f62cd689baf21d4391d45de253
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-08-16 01:45:19 +1000

    feat: connect apps with packages

--- apps/react-playground/vite.config.ts
+++ apps/react-playground/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const projectRoot = resolve(__dirname, "../..");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: /^@namui\/(.*)/,
+        replacement: resolve(projectRoot, "packages", "$1"),
+      },
+    ],
+  },
+});
---
+++ packages/react/components/button/src/button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Button() {
+  return <button>I am a button from @namui/react/components/button</button>;
+}
+
+export { Button };
